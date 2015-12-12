adf1770f25400f783196ec43f8fb974dbea48b42
Christopher Jenkins <christj@gmail.com>
2015-12-12 10:50:49 -0600

    fix dir name

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,12 @@
 class get_iplayer::config inherits get_iplayer {
 
   file { 'get_iplayer.profile_sh':
-    path    => '/etc/profile/get_iplayer.sh',
+    path    => '/etc/profile.d/get_iplayer.sh',
     content => "export GETIPLAYERUSERPREFS=${get_iplayer::conf_dir}\n",
   }
 
   file { 'get_iplayer.profile_csh':
-    path    => '/etc/profile/get_iplayer.csh',
+    path    => '/etc/profile.d/get_iplayer.csh',
     content => "setenv GETIPLAYERUSERPREFS ${get_iplayer::conf_dir}\n",
   }
 
