cd3bd69cec16c3d3518d4cef72dd55bced0e71bd
Christopher Jenkins <christj@gmail.com>
2015-12-12 10:48:12 -0600

    add environment variables for conf dir

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,13 @@
 class get_iplayer::config inherits get_iplayer {
 
+  file { 'get_iplayer.profile_sh':
+    path    => '/etc/profile/get_iplayer.sh',
+    content => "export GETIPLAYERUSERPREFS=${get_iplayer::conf_dir}\n",
+  }
+
+  file { 'get_iplayer.profile_csh':
+    path    => '/etc/profile/get_iplayer.csh',
+    content => "setenv GETIPLAYERUSERPREFS ${get_iplayer::conf_dir}\n",
+  }
+
 }
