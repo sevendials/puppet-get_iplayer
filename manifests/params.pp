9ee032d7a8ec9449aa5c5f1183df0952465e3d53
Christopher Jenkins <christj@gmail.com>
2015-12-15 10:40:33 -0600

    Fix upstart script name

--- manifests/params.pp
+++ manifests/params.pp
@@ -35,7 +35,7 @@ class get_iplayer::params {
         }
         '6': {
           $service_template = 'get_iplayer/init.erb'
-          $service_path     = '/etc/init/get_iplayer'
+          $service_path     = '/etc/init/get_iplayer.conf'
           $service_provider = 'upstart'
 
           $prereqs  = {
