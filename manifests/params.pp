4d4469d5072ec6131f08f06dfcc61f62a4e9755d
Christopher Jenkins <christj@gmail.com>
2015-12-15 09:04:23 -0600

    Create separate params for rhel6

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,34 +12,55 @@ class get_iplayer::params {
 
   case $::osfamily {
     "RedHat": {
-      if versioncmp($::operatingsystemrelease, '7') >= 0 {
-        $service_template = 'get_iplayer/systemd.erb'
-        $service_path     = '/lib/systemd/system/get_iplayer.service'
-      }
-      else {
-        $service_template = 'get_iplayer/init.erb'
-        $service_path     = '/etc/init.d/get_iplayer'
+      case $::operatingsystemrelease {
+        "7": {
+          $service_template = 'get_iplayer/systemd.erb'
+          $service_path     = '/lib/systemd/system/get_iplayer.service'
+          $prereqs  = {
+            "perl" => {},
+            "perl-CGI" => {},
+            "perl-Env" => {},
+            "perl-HTML-Parser" => {},
+            "perl-HTTP-Cookies" => {},
+            "perl-libwww-perl" => {},
+            "perl-XML-Simple" => {},
+            "ffmpeg" => { require => 'Package[nux-dextop-release]' },
+            'rtmpdump' => { require => 'Package[nux-dextop-release]' },
+            'nux-dextop-release' => {
+              'source'    => 'http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm',
+              'provider'  => 'rpm',
+              'require'   => 'Package[epel-release]',
+            },
+          }
+        }
+        '6': {
+          $service_template = 'get_iplayer/init.erb'
+          $service_path     = '/etc/init.d/get_iplayer'
+
+          $prereqs  = {
+            "perl" => {},
+            "perl-CGI" => {},
+            "perl-Env" => {},
+            "perl-HTML-Parser" => {},
+            "perl-HTTP-Cookies" => {},
+            "perl-libwww-perl" => {},
+            "perl-XML-Simple" => {},
+            "ffmpeg" => { require => 'Package[nux-dextop-release]' },
+            'rtmpdump' => { require => 'Package[nux-dextop-release]' },
+            'nux-dextop-release' => {
+              'source'    => 'http://li.nux.ro/download/nux/dextop/el6/x86_64/nux-dextop-release-0-2.el6.nux.noarch.rpm',
+              'provider'  => 'rpm',
+              'require'   => 'Package[epel-release]',
+            },
+          }
+        }
+        default: {
+          err("OS ${::operatingsystem} ${::operatingsystemrelease} is not supported")
+        }
       }
 
       $service_env_template = 'get_iplayer/service_env.erb'
       $service_env_path     = '/etc/sysconfig/get_iplayer'
-
-      $prereqs  = {
-        "perl" => {},
-        "perl-CGI" => {},
-        "perl-Env" => {},
-        "perl-HTML-Parser" => {},
-        "perl-HTTP-Cookies" => {},
-        "perl-libwww-perl" => {},
-        "perl-XML-Simple" => {},
-        "ffmpeg" => { require => 'Package[nux-dextop-release]' },
-        'rtmpdump' => { require => 'Package[nux-dextop-release]' },
-        'nux-dextop-release' => {
-          'source'    => 'http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm',
-          'provider'  => 'rpm',
-          'require'   => 'Package[epel-release]',
-        },
-      }
 
     }
     default: {
