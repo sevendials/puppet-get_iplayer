f0a1ceb3278bd78111fb812bd81b9c571351cbb3
Christopher Jenkins <christj@gmail.com>
2015-12-16 12:17:55 -0600

    Call validation functions on class parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,6 +50,21 @@ class get_iplayer (
   $version                = $get_iplayer::params::version,
 ) inherits get_iplayer::params {
 
+  validate_absolute_path($base_dir)
+  validate_absolute_path($conf_dir)
+  validate_ipv4_address($listen_address)
+  validate_integer($listen_port)
+  validate_bool($manage_prereqs)
+  validate_hash($options_extra)
+  validate_absolute_path($output_dir)
+  validate_hash($prereqs)
+  validate_absolute_path($service_env_path)
+  validate_string($service_env_template)
+  validate_absolute_path($service_path)
+  validate_string($service_template)
+  validate_string($source_location)
+  validate_numeric($version)
+
   anchor { 'get_iplayer::begin': } ->
   class { '::get_iplayer::install': } ->
   class { '::get_iplayer::config': } ~>
