3c884b7233000c1cb16b70664fd42672b3924f12
Christopher Jenkins <christj@gmail.com>
2015-12-19 07:03:33 -0600

    Start using puppet lookup (data in modules) as introduced in 4.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 class get_iplayer (
-  $base_dir               = $get_iplayer::params::base_dir,
-  $conf_dir               = $get_iplayer::params::conf_dir,
+  $base_dir,
+  $conf_dir,
   $listen_address         = $get_iplayer::params::listen_address,
   $listen_port            = $get_iplayer::params::listen_port,
   $manage_prereqs         = $get_iplayer::params::manage_prereqs,
   $options_extra          = {},
-  $output_dir             = $get_iplayer::params::output_dir,
+  $output_dir,
   $prereqs                = $get_iplayer::params::prereqs,
   $service_env_path       = $get_iplayer::params::service_env_path,
   $service_env_template   = $get_iplayer::params::service_env_template,
   $service_path           = $get_iplayer::params::service_path,
   $service_template       = $get_iplayer::params::service_template,
-  $source_location        = $get_iplayer::params::source_location,
-  $version                = $get_iplayer::params::version,
+  $source_location,
+  $version,
 ) inherits get_iplayer::params {
 
   validate_absolute_path($base_dir)
