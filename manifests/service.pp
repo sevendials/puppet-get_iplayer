bf30e20913bf69f9cc7347c667f1524f8c3c91e1
Christopher Jenkins <christj@gmail.com>
2015-12-20 17:06:37 -0600

    Fix lint errors

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,14 @@
 class get_iplayer::service inherits get_iplayer {
 
+  $provider = $get_iplayer::service_provider ? {
+    ''      => undef,
+    default => $get_iplayer::service_provider,
+  }
+
   service { 'get_iplayer':
-    ensure    => 'running',
-    enable    => true,
-    provider  => $get_iplayer::service_provider ? {
-      ''      => undef,
-      default => $get_iplayer::service_provider,
-    },
+    ensure   => 'running',
+    enable   => true,
+    provider => $provider,
   }
 
 }
