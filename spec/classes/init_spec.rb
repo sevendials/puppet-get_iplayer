6a72a73e8e27357fc33fa1f74f08804430155bd6
Christopher Jenkins <christj@gmail.com>
2016-03-30 17:58:56 -0700

    add facts to spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 describe 'get_iplayer' do
 
   context 'with defaults for all parameters' do
+    let(:facts) { {
+      :operatingsystem        => 'Ubuntu',
+      :ipaddress              => '192.168.0.1',
+      :osfamily               => 'Debian',
+      :operatingsystemrelease => '14.04',
+      :os                     => {'release' => { 'major' => '14.04'}}
+    } }
     it { should contain_class('get_iplayer') }
   end
 end
