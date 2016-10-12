15d8692fca8c5f34e948d2778852de2d441c1ab2
Christopher Jenkins <christj@gmail.com>
2016-10-12 15:10:31 -0700

    cover all resources in spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,11 +11,24 @@ describe 'get_iplayer' do
       :staging_http_get       => 'curl',
       :path                   => '/opt',
     } }
+    let(:params) { {
+      version: '2.94',
+    } }
     it { should contain_class('get_iplayer') }
     it { should contain_class('get_iplayer::install') }
     it { should contain_class('get_iplayer::config') }
     it { should contain_class('get_iplayer::service') }
     it { should contain_class('get_iplayer::params') }
+    it { should contain_anchor('get_iplayer::begin') }
+    it { should contain_anchor('get_iplayer::end') }
+    it { should contain_file('get_iplayer.bin') }
+    it { should contain_file('get_iplayer.conf_dir') }
+    it { should contain_file('get_iplayer.options') }
+    it { should contain_file('get_iplayer.output_dir') }
+    it { should contain_file('get_iplayer.service') }
+    it { should contain_file('get_iplayer.service_env') }
+    it { should contain_service('get_iplayer') }
+    it { should contain_staging__deploy('v2.94.zip') }
   end
 
   at_exit { RSpec::Puppet::Coverage.report! }
